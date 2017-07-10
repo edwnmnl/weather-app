688d2bc3952b3ccb9570abe54e351353b6faa543
Edwin <edwn.mnl@gmail.com>
2017-07-10 10:42:28 +0530

    Added public directory and CSS file.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
 	res.render('index')
-})
+});
 
 app.listen(3000, function() {
 	console.log('Server listening on port 3000.')
-})
+});

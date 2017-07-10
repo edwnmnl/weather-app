d730ce3a0a3d39b46ce650c3a10e12299d609450
Edwin <edwn.mnl@gmail.com>
2017-07-10 11:14:49 +0530

    Added request and POST route.

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
+const request = require('request');
+const app = express();
+const apiKey  = 'abc0d5799fd3d158de9bd3d7821a9e1a';
 
 app.use(express.static('public'));
 
@@ -13,8 +15,32 @@ app.get('/', function(req, res) {
 });
 
 app.post('/', function(req, res) {
-	res.render('index');
-	console.log(req.body.city);
+	let city = req.body.city;
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+
+	request(url, function(err, response, body) {
+		if (err) {
+			res.render('index', {
+				weather: null,
+				error: 'Error, please try agin later'
+			});
+		} else {
+			let weather = JSON.parse(body);
+			if (weather.main == undefined) {
+				res.render('index', {			
+					weather: null,
+					error: 'Error, please try again later'
+				});
+			} else {
+				let weatherText = 'Its ' + weather.main.temp + ' degrees in ' + weather.name + '!';
+				res.render('index', {
+					weather: weatherText,
+					error: null
+				});
+			}
+
+		}
+	});
 });
 
 app.listen(3000, function() {
